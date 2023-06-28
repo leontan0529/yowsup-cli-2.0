5919f39b12a819473fead1f98e31f19f7e19d663
Leon Tan <73455747+leontan0529@users.noreply.github.com>
2023-06-28 17:28:12 +0800

    env_utilities.py

--- yowsup/env/env_utilities.py
+++ yowsup/env/env_utilities.py
@@ -1,3 +1,6 @@
+#Done by Leon Tan 28/06/23
+#Updated auto-update env_android.py variables based on apk version
+
 from pyaxmlparser import APK
 import sys
 import base64
@@ -15,6 +18,16 @@ try:
     classesDexFile = zipFile.read('classes.dex')
     hash = hashlib.md5()
     hash.update(classesDexFile)
+    ver = apk.version_name
+    md5 = base64.b64encode(hash.digest()).decode("utf-8")
+
+    with open("env_android.py") as file:
+        lines = file.readlines()
+    lines[19] = "    _MD5_CLASSES = " + '"{0}"'.format(md5) + "\n"
+    lines[21] = "    _VERSION = " + '"{0}"'.format(ver) + "\n"
+    with open("cp_env_android.py", "w") as file:
+        for line in lines:
+            file.write(line)
 
     print("Version: " + apk.version_name)
     print("ClassesDex: " + base64.b64encode(hash.digest()).decode("utf-8"));
